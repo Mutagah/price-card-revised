a7f877e0ad91c504c8f11bc02e3d25777790f774
Mutagah <kihahumutagah@gmail.com>
2024-03-14 10:06:12 +0300

    :fix:UI style fixes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'price_cards';
+
+  priceCards = [
+    {
+      name: 'Starter',
+      price: '$55',
+      features: [
+        '500 MAUS',
+        '3 projects',
+        'Unlimited guides',
+        'Unlimited flows',
+        'Unlimited branded themes',
+        'Email support',
+      ],
+    },
+    {
+      name: 'Pro',
+      price: '$99',
+      rotation:'/month',
+      features: [
+        'All starter features, plus:',
+        'Unlimited Projects',
+        'Unlimited fully customizable themes',
+        'A dedicated Customer Success Manager',
+      ],
+    },
+    {
+      name: 'Enterprise',
+      price: "Let's Talk!",
+      features: [
+        'All pro features',
+        'Unlimited MAUs',
+        'Dedicated Environment',
+        'Dedicated Enterprise account administration',
+        'Premium support and services',
+      ],
+    },
+  ];
 }
